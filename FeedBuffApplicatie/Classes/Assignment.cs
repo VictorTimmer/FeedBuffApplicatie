1a978ef4ad3ce47f2c6d673b144a71169736efc2
Spamc <Spamc@VICTORY>
2023-04-06 17:48:13 +0200

    Classes finsished +Base

---
+++ FeedBuffApplicatie/Classes/Assignment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeedBuffApplicatie.Classes
+{
+    public class Assignment
+    {
+        public int Id { get; } 
+
+        public string Description { get; set; }
+        public DateTime Deadline { get; set; }
+        public string Completed {get; set; }
+
+
+
+        public Assignment(int id, string description, DateTime deadline, string completed)
+        {
+            Id = id;
+            Description = description;
+            Deadline = deadline;
+            Completed = completed;
+        }
+
+        public void AddAssigment()
+        { 
+        
+        }
+    }
+}
---
+++ FeedBuffApplicatie/Classes/Supervisor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeedBuffApplicatie.Classes
+{
+    public class Supervisor
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int WorkNumber { get; set; }
+
+        public Supervisor(int id, string firstname, string lastname, string username, string password, int workNumber)
+        {
+            Id = id;
+            Firstname = firstname;
+            Lastname = lastname;
+            Username = username;
+            Password = password;
+            WorkNumber = workNumber;
+        }
+
+
+        public void AddSupervisor()
+        { 
+
+        }
+    }
+}
