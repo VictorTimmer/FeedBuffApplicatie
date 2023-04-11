602410c533fad2870ec517f16403fc63e49d7055
Max <vandenbergmax@outlook.com>
2023-04-11 23:13:55 +0200

    Adding new Assignment CRUD form

--- FeedBuffApplicatie/Forms/FormFeedforward.Designer.cs
+++ FeedBuffApplicatie/Forms/FormFeedforward.Designer.cs
@@ -28,21 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonDiscard = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.dgvAssignment = new System.Windows.Forms.DataGridView();
+            this.textBoxDescription = new System.Windows.Forms.TextBox();
+            this.labelDescription = new System.Windows.Forms.Label();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.dateTimePickerDate = new System.Windows.Forms.DateTimePicker();
+            this.labelCompleted = new System.Windows.Forms.Label();
+            this.checkBoxCompleted = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAssignment)).BeginInit();
             this.SuspendLayout();
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(502, 328);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(86, 23);
+            this.buttonDelete.TabIndex = 0;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            // 
+            // buttonDiscard
+            // 
+            this.buttonDiscard.Location = new System.Drawing.Point(410, 328);
+            this.buttonDiscard.Name = "buttonDiscard";
+            this.buttonDiscard.Size = new System.Drawing.Size(86, 23);
+            this.buttonDiscard.TabIndex = 1;
+            this.buttonDiscard.Text = "Discard";
+            this.buttonDiscard.UseVisualStyleBackColor = true;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(502, 299);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(86, 23);
+            this.buttonSave.TabIndex = 2;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(410, 299);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(86, 23);
+            this.buttonCreate.TabIndex = 3;
+            this.buttonCreate.Text = "Create";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            // 
+            // dgvAssignment
+            // 
+            this.dgvAssignment.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAssignment.GridColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.dgvAssignment.Location = new System.Drawing.Point(12, 12);
+            this.dgvAssignment.Name = "dgvAssignment";
+            this.dgvAssignment.Size = new System.Drawing.Size(576, 241);
+            this.dgvAssignment.TabIndex = 4;
+            // 
+            // textBoxDescription
+            // 
+            this.textBoxDescription.Location = new System.Drawing.Point(87, 272);
+            this.textBoxDescription.Name = "textBoxDescription";
+            this.textBoxDescription.Size = new System.Drawing.Size(200, 20);
+            this.textBoxDescription.TabIndex = 5;
+            // 
+            // labelDescription
+            // 
+            this.labelDescription.AutoSize = true;
+            this.labelDescription.Location = new System.Drawing.Point(12, 276);
+            this.labelDescription.Name = "labelDescription";
+            this.labelDescription.Size = new System.Drawing.Size(60, 13);
+            this.labelDescription.TabIndex = 6;
+            this.labelDescription.Text = "Description";
+            // 
+            // labelDate
+            // 
+            this.labelDate.AutoSize = true;
+            this.labelDate.Location = new System.Drawing.Point(12, 307);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(49, 13);
+            this.labelDate.TabIndex = 7;
+            this.labelDate.Text = "Deadline";
+            // 
+            // dateTimePickerDate
+            // 
+            this.dateTimePickerDate.Location = new System.Drawing.Point(87, 303);
+            this.dateTimePickerDate.Name = "dateTimePickerDate";
+            this.dateTimePickerDate.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerDate.TabIndex = 8;
+            // 
+            // labelCompleted
+            // 
+            this.labelCompleted.AutoSize = true;
+            this.labelCompleted.Location = new System.Drawing.Point(12, 336);
+            this.labelCompleted.Name = "labelCompleted";
+            this.labelCompleted.Size = new System.Drawing.Size(57, 13);
+            this.labelCompleted.TabIndex = 9;
+            this.labelCompleted.Text = "Completed";
+            // 
+            // checkBoxCompleted
+            // 
+            this.checkBoxCompleted.AutoSize = true;
+            this.checkBoxCompleted.Location = new System.Drawing.Point(87, 335);
+            this.checkBoxCompleted.Name = "checkBoxCompleted";
+            this.checkBoxCompleted.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxCompleted.TabIndex = 10;
+            this.checkBoxCompleted.UseVisualStyleBackColor = true;
             // 
             // FormFeedforward
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Yellow;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Coral;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.checkBoxCompleted);
+            this.Controls.Add(this.labelCompleted);
+            this.Controls.Add(this.dateTimePickerDate);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.labelDescription);
+            this.Controls.Add(this.textBoxDescription);
+            this.Controls.Add(this.dgvAssignment);
+            this.Controls.Add(this.buttonCreate);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonDiscard);
+            this.Controls.Add(this.buttonDelete);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FormFeedforward";
             this.Text = "FormFeedforward";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAssignment)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonDiscard;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.DataGridView dgvAssignment;
+        private System.Windows.Forms.TextBox textBoxDescription;
+        private System.Windows.Forms.Label labelDescription;
+        private System.Windows.Forms.Label labelDate;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDate;
+        private System.Windows.Forms.Label labelCompleted;
+        private System.Windows.Forms.CheckBox checkBoxCompleted;
     }
 }
