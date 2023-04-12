63533d5a060c99e075164502e26b7e9265bfd519
Ruben Lomme <1248510lomme@zuyd.nl>
2023-04-12 12:37:13 +0200

    Added DAL classes

---
+++ FeedBuffApplicatie/DAL/FeeditemDAL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeedBuffApplicatie.DAL
+{
+    internal class FeeditemDAL
+    {
+    }
+}
